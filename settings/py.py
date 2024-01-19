5cb0668b6801f02e9083cabbadd017ae75aa868d
GuilhermeSilva <biojgs@gmail.com>
2024-01-18 21:19:39 -0300

    feat: getting values from page

--- settings/py.py
+++
@@ -1,8 +0,0 @@
-url_data = {
-    'sauceDemo': 'https://www.saucedemo.com/v1/',
-    'sauceDemo1': 'https://www.saucedemo.com/v2/',
-    'sauceDemo3': 'https://www.saucedemo.com/v3/'
-}
-
-for itens in url_data:
-    print (itens)
